3188a7e2c40c8d58deb124f682ef5b0c2da07da2
tamasfe <me@tamasfe.dev>
2025-03-18 21:14:16 +0100

    perf(sqlite): schedule query

--- crates/ora-storage-sqlite/src/schedule_query.rs
+++ crates/ora-storage-sqlite/src/schedule_query.rs
@@ -193,19 +193,10 @@ pub(super) fn query_schedule_details(
                         SELECT
                             1
                         FROM
-                            "ora_job"
+                            "ora_schedule_job_state" js
                         WHERE
-                            marked_unschedulable_at_unix_ns IS NULL
-                            AND "schedule_id" = "ora_schedule"."id"
-                            AND EXISTS (
-                                SELECT
-                                    1
-                                FROM
-                                    "ora_execution"
-                                WHERE
-                                    "status" IN (0, 1, 2)
-                                    AND "job_id" = "ora_job"."id"
-                            )
+                            js."schedule_id" = "ora_schedule"."id"
+                            AND js."active_job_id" IS NOT NULL
                     )
                 ) AS "active",
                 "cancelled_at_unix_ns",
